48f21f54f1d129e0fcdb88ed95df077f34d9c327
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2021-02-03 00:40:10 -0300

    Initial commit

---
+++ src/main/java/com/carledwinti/LibraryAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.carledwinti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAdminServerApplication.class, args);
+	}
+
+}
