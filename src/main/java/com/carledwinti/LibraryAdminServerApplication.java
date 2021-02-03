9bca28a57289c2b0db25157055f645ece150a5ac
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2021-02-03 01:00:41 -0300

    Configuração

--- src/main/java/com/carledwinti/LibraryAdminServerApplication.java
+++ src/main/java/com/carledwinti/LibraryAdminServerApplication.java
@@ -1,9 +1,11 @@
 package com.carledwinti;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class LibraryAdminServerApplication {
 
 	public static void main(String[] args) {
